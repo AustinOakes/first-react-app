7cf01fc018f8db35368a7410fb4c61e6d72a2777
Austin Oakes <aoakes208@gmail.com>
2024-02-13 23:48:02 -0500

    added createElement example

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ function Greeting() {
     return <h2>My First Component</h2>
 }
 
+function Greeting2() {
+    return React.createElement('h2', null, 'Hello World!');
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greeting/>)
