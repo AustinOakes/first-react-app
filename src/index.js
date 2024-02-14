f043aec77812099ad009d42e72ea1ba3c66cdbf1
Austin Oakes <aoakes208@gmail.com>
2024-02-14 00:02:48 -0500

    Went through JSX rules - Start Nesting Components section next

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return <h2>My First Component</h2>
+    return (
+        <React.Fragment>
+            <h2>My First Component</h2>
+        </React.Fragment>
+    );
 }
 
 function Greeting2() {
