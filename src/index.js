b9e98803ed67321b6a597d6153ed0dcc36b88534
Austin Oakes <aoakes208@gmail.com>
2024-02-22 19:43:14 -0500

    started booklist

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
+const BookList = () => {
     return (
-        <div>
-            <Person/>
-            <Message/>
-        </div>
-    );
+        <section>
+            <Book/>
+        </section>
+    )
 }
 
-const Person = () => <h2>My First Component</h2>;
-const Message = () => { 
-    return <p>This is my message</p>; 
+const Book = () => {
+    return <article>
+        <Image/>
+        <Title/>
+        <Author/>
+    </article>
 }
 
 
-
+const Image = () => <h2>Image Placeholder</h2>
+const Title = () => <h2>Book Title</h2>
+const Author = () => <h4>Author</h4>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>)
+root.render(<BookList/>)
