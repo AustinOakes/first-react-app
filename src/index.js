6e2d7bebf79a6e1921cce7872324a4056553514d
Austin Oakes <aoakes208@gmail.com>
2024-02-24 02:55:00 -0500

    Adjusted the app to use props

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// importing css
 import './index.css';
+
+const firstBook = {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg'
+}
+
+const secondBook = {
+    author: 'Tony Robbins',
+    title: 'The Holy Grail of Investing',
+    img: 'https://m.media-amazon.com/images/I/71zQ-0+fV7L.jpg'
+}
+
+const author = 'James Clear';
+const title = 'Atomic Habits';
+const img = 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg'
 
 const BookList = () => {
     return (
         <section className='booklist'>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
+            <Book author = {firstBook.author} title = {firstBook.title} img={firstBook.img}/>
+            <Book author = {secondBook.author} title = {secondBook.title} img={secondBook.img}/>
         </section>
     )
 }
 
-const Book = () => {
+const Book = (props) => {
+    console.log(props);
     return <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
+        <img 
+            src = {props.img} 
+            alt = {props.title}
+        />
+        <h2>{props.title}</h2>
+        <h4>{props.author}</h4>
     </article>
 }
 
-
-const Image = () => <img src = "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg" alt='Atomic Habits'/>
-const Title = () => <h2>Atomic Habits</h2>
-const Author = () => {
-    const inlineHeadingStyles = { color: '#617d98', foontSize: '0.75rem', marginTop: '0.5rem', };
-    return <h4 style = {inlineHeadingStyles}>James Clear</h4>
-}
+//Start with child props next
 
 
-// Start with props next
-
-
+// Gets the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Renders the Booklist component on the root element
 root.render(<BookList/>)
