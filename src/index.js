c93ea253ca71da872aad81c2227f261c9078544c
Austin Oakes <aoakes208@gmail.com>
2024-02-23 21:57:01 -0500

    added some CSS & book details to the book list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './index.css';
+
 const BookList = () => {
     return (
-        <section>
+        <section className='booklist'>
+            <Book/>
+            <Book/>
+            <Book/>
             <Book/>
         </section>
     )
 }
 
 const Book = () => {
-    return <article>
+    return <article className='book'>
         <Image/>
         <Title/>
         <Author/>
@@ -18,9 +23,9 @@ const Book = () => {
 }
 
 
-const Image = () => <h2>Image Placeholder</h2>
-const Title = () => <h2>Book Title</h2>
-const Author = () => <h4>Author</h4>
+const Image = () => <img src = "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg" alt='Atomic Habits'/>
+const Title = () => <h2>Atomic Habits</h2>
+const Author = () => <h4>James Clear</h4>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
