37e264c57836ab954d447f41f60599dc560560bd
Austin Oakes <aoakes208@gmail.com>
2024-03-17 15:55:54 -0400

    started adding events

--- src/index.js
+++ src/index.js
@@ -20,31 +20,66 @@ const books = [
 ];
 
 const BookList = () => {
+    const someValue = 'shakeandbake';
+    const displayValue = () => {
+        console.log(someValue);
+    }
     return (
         <section className='booklist'>
+            {/* <EventExamples/> */}
             {books.map((book)=>{
                 return(
-                    <Book {...book} key={book.id}/>
+                    <Book {...book} key={book.id} displayValue={displayValue}/>
                 );
             })}
         </section>
     )
 }
 
+
+// const EventExamples = () => {
+//     const handleFormInput = (e) => {
+//         console.log(e.target);
+//         console.log(e.target.name);
+//         console.log(e.target.value);
+//     };
+//     const handleButtonClick = (e) => {
+//         alert('handle button click');
+//     };
+//     const handleFormSubmission = (e) => {
+//         e.preventDefault();
+//         console.log('form submitted')
+//     };
+//     return <section>
+//         <form onSubmit={handleFormSubmission}>
+//             <h2>Typical Form</h2>
+//             <input type='text' name='example' onChange={handleFormInput} style={{margin: '1rem 0'}}/>
+//         </form>
+//         <button onClick={handleButtonClick}>Click me</button>
+//     </section>
+// }
+
+
 const Book = (props) => {
     console.log(props);
-    const { img, title, author } = props;
+    const { img, title, author, displayValue } = props;
+
+    const displayTitle = () => {
+        console.log(title);
+    };
+
     return <article className='book'>
         <img 
             src = {img} 
             alt = {title}
         />
         <h2>{title}</h2>
+        <button onClick={displayValue}>Click Me</button>
         <h4>{author}</h4>
     </article>
 }
 
-//Start with Events section next
+// Start again at 3:35:57 - This should be some sort of challenge
 
 
 // Gets the root element
